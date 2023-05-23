52d542783e068bcb60e0696cafb4961de55845f7
iambelloopeyemi <opeyemiemmanuelbello@gmail.com>
2023-05-23 17:00:59 +0100

    implemented the auth pages routing and organize the project folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+// layouts
+import LogInLayout from "./layouts/Auth/LogInLayout";
+import SignUpLayout from "./layouts/Auth/SignUpLayout";
+import DashboardLayout from "./layouts/Dashboard/DashboardLayout";
+// pages
+import LogIn from "./Pages/Auth/LogIn/LogIn";
+import SignUp from "./Pages/Auth/SignUp/SignUp";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<LogInLayout />}>
+      <Route index element={<LogIn />} />
+      <Route path="sign-up" element={<SignUpLayout />}>
+        <Route index element={<SignUp />} />
+      </Route>
+      <Route path="dashboard" element={<DashboardLayout />} >
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Route>
+  )
+);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
