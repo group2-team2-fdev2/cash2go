15f5d0bd8e9e1bcbedaca3376d52f59ae2c94c86
omolade <tosin.ogunmola44@gmail.com>
2023-05-28 13:43:56 +0100

    email and company setup

--- src/Pages/Auth/SignUp/EmailAndCompanyIDAuth/EmailAndCompanyIDAuth.jsx
+++ src/Pages/Auth/SignUp/EmailAndCompanyIDAuth/EmailAndCompanyIDAuth.jsx
@@ -1,10 +1,47 @@
+import { Link } from "react-router-dom";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import "./EmailAndCompanyIDAuth.css"
 
 
 export default function EmailAndCompanyIDAuth() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+
   return (
     <div>
-      
+      <div className="signup_container">
+        <h2 className="signup_heading">Sign Up</h2>
+        <form className="signup_form" onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="myworkemail@work.com"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="company_id">Company ID</label>
+            <input
+              type="text"
+              name="company_id"
+              id="company_id"
+              placeholder="123ABC"
+            />
+          </div>
+          <button className="form_btn">
+            Next <AiOutlineArrowRight />
+          </button>
+        </form>
+        <div className="attribution">
+          <Link to="">Terms of use</Link>
+          <Link to="">Privacy Policy</Link>
+        </div>
+           
+      </div>
     </div>
-  )
+  );
 }
-
