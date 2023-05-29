5d1b7f6c9683df0cd164d42a93ff603db175a7e7
omolade <tosin.ogunmola44@gmail.com>
2023-05-29 01:14:59 +0100

    left sign up page

---
+++ src/Pages/Auth/SignUp/Components/SignUpFormSuccess.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../EmailAndCompanyIDAuth/EmailAndCompanyIDAuth.css"
+
+const SignUpFormSuccess = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignUpFormSuccess
