7ea20811cd68e9ec431bc2e57b3cb099bdaaa16b
Asonwa Nnabeze <lilynnabeze@gmail.com>
2023-06-02 00:52:05 +0100

    email request and security question pages for password reset fully authenticated, sidebar component created for the dashboard.

---
+++ src/Pages/Dashboard/components/LogoutIcon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function LogoutIcon() {
+  return (
+    <div className="logout-container">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="icons"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
+        />
+      </svg>
+      <p>LOG OUT</p>
+    </div>
+  );
+}
