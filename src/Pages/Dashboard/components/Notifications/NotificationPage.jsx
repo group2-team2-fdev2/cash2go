522b59e46097cc85e5fe36e1fd0eec30bfaaa05e
Milad-Rufai <temilader19@gmail.com>
2023-06-11 20:03:28 +0100

    Notifications Objects

--- src/Pages/Dashboard/components/Notifications/NotificationPage.jsx
+++ src/Pages/Dashboard/components/Notifications/NotificationPage.jsx
@@ -1,21 +1,31 @@
-import Notification from "./notification";
+
+import { Notifications } from './NotificationObj';
+import OptionsIcon from './OptionsIcon';
 import './notification.css'
 
 export default function NotificationPage () {
 
   return (
     <div className="display">
-      <h1>Notifications</h1>
       <div className="notification-title">
       <h4>All Notifications</h4>
       <p>Sorted by Date</p>
       </div>
-      <Notification title='Security Alert' message="There was a login attempt into your account. Kindly change your login password if that was not you" />
-      <Notification title='Application Report' message="The short term loan of Ogbeni Mallam Stitern (003556Z) just got approved with a 70% prediction metric" />
-      <Notification title='Application Download' message="You just downloaded the application of Ogbeni Mallam Stitern (003556Z)" />
-      <Notification title='Application Report' message="The short term loan of Ogbeni Mallam Stitern (003557Z) just got rejected with a 50% prediction metric" />
-      <Notification title='System Updates' message="A new web app version 5.0 will be launched by 30th of May" />
-      <Notification title='New Resources' message="A new tutorial has been published in the resources channel" />
+      <div>
+      {Notifications.map((Notifications) => {
+            return (
+      <div className="notification" key={Notifications.id}>
+        <div>
+            <div className='date'>{new Date().toLocaleString() }</div>
+            <div className='title'>{Notifications.title}</div> 
+          <div className='message'>{Notifications.message}</div>
+          </div>
+          <div className='icon'>
+          <span>{<OptionsIcon/>}</span>
+          </div>
+        </div> )
+          })} 
+        </div>
     </div>
   );
 }
---
+++ src/Pages/Dashboard/components/Notifications/OptionsIcon.jsx
@@ -0,0 +1,21 @@
+export default function OptionsIcon() {
+    return (
+      <>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+          />
+        </svg>
+      </>
+    );
+  }
+  
