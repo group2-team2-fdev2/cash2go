e7476f908f8e4b8fb7f277e56cbc754770e5dd20
Milad-Rufai <temilader19@gmail.com>
2023-06-21 20:31:48 +0100

    changes

--- src/Pages/Dashboard/Notification/Notification.jsx
+++ src/Pages/Dashboard/Notification/Notification.jsx
@@ -60,7 +60,6 @@ export default function NotificationPage() {
                     </div>
                     <div className="Notification-notifications-date">
                       {new Date().toLocaleString()}
-                      
                     </div>
 
                     <div className="Notification-messages-title">
@@ -112,7 +111,7 @@ export default function NotificationPage() {
               disabled={indexOfLastItem >= Notifications.length}
               onClick={() => paginate(currentPage + 1)}
             >
-             <span> {<NextArrow />}</span>
+              <span> {<NextArrow />}</span>
             </button>
           </div>
         </div>
