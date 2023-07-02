c54499430b1448c2a23772be2b0ae8871e972db7
Milad-Rufai <temilader19@gmail.com>
2023-07-02 14:48:16 +0100

    Notification Page Update

--- src/Pages/Dashboard/components/Notifications/NotificationObj.jsx
+++ src/Pages/Dashboard/components/Notifications/NotificationObj.jsx
@@ -4,7 +4,7 @@ export const Notifications = [
      {
     id: nanoid(),
     title: 'Welcome to Cash2go',
-    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim rem fugiat sint ea adipisci porro aliquid maxime maiores minima sunt atque nostrum, culpa alias distinctio cum unde doloribus natus. Officiis.     Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim rem fugiat sint ea adipisci porro aliquid maxime maiores minima sunt atque nostrum, culpa alias distinctio cum unde doloribus natus. Officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim rem fugiat sint ea adipisci porro aliquid maxime maiores minima sunt atque nostrum, culpa alias distinctio cum unde doloribus natus. Officiis.     Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim rem fugiat sint ea adipisci porro aliquid maxime maiores minima sunt atque nostrum, culpa alias distinctio cum unde doloribus natus. Officiis."
+    message: "Thank you for choosing our app. We're excited to have you on board and hope you have a great experience using our services. With cash2go, you can unlock a world of possibilities and enjoy a seamless experience tailored just for you.         Our mission is to provide you with the best tools and features to help you achieve your goals and make your life easier. Whether you're managing simple lending projects or tasks, cash2go has got you covered. If you have any questions or need assistance, feel free to reach out to our support team. Enjoy exploring the app!"
 
 }
 ]
