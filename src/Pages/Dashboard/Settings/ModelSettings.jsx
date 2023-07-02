48ec7ba931599037788b692d03f17de710280465
Uloma <lommyjj@gmail.com>
2023-07-02 07:16:50 +0100

    updated the settings

--- src/Pages/Dashboard/Settings/ModelSettings.jsx
+++ src/Pages/Dashboard/Settings/ModelSettings.jsx
@@ -6,10 +6,26 @@ import InactiveStatus from "./InactiveStatus";
 
 export default function ModelSettings() {
   const [toggleActive1, setToggleActive1] = useState(false);
+  const [toggleActive2, setToggleActive2] = useState(false);
+  const [toggleActive3, setToggleActive3] = useState(false);
+  const [toggleActive4, setToggleActive4] = useState(false);
 
   const handleActiveToggle = (toggleActive, setToggleActive) => {
     setToggleActive(!toggleActive);
   };
+
+  const handleActiveToggle2 = (toggleActive2, setToggleActive2) => {
+    setToggleActive2(!toggleActive2);
+  };
+
+  const handleActiveToggle3 = (toggleActive3, setToggleActive3) => {
+    setToggleActive3(!toggleActive3);
+  };
+
+  const handleActiveToggle4 = (toggleActive4, setToggleActive4) => {
+    setToggleActive4(!toggleActive4);
+  };
+
   return (
     <table className="Settings-table">
       <thead className="Settings-thead">
@@ -68,6 +84,139 @@ export default function ModelSettings() {
           <td className="Settings-tbody_data">
             <TbEdit className="Edit-icon" />
           </td>
+
+        </tr>
+
+
+        <tr>
+          <td className="Settings-tbody_data">
+            <div className="Settings-tbody_model">
+              <div
+                onClick={() =>
+                  handleActiveToggle2(toggleActive2, setToggleActive2)
+                }
+              >
+                {toggleActive2 ? (
+                  <BsToggleOff className="ToggleOff-icon" />
+                ) : (
+                  <BsToggleOn className="ToggleOn-icon" />
+                )}
+              </div>
+              <div>
+                <p className="Settings-tbody_model_title">Alpha Model</p>
+                <p className="Settings-tbody_model_subtitle">
+                  Priotize Prev. Loans
+                </p>
+              </div>
+            </div>
+          </td>
+          <td className="Settings-tbody_data">
+            <div
+              onClick={() =>
+                handleActiveToggle2(toggleActive2, setToggleActive2)
+              }
+            >
+              {toggleActive2 ? <InactiveStatus /> : <ActiveStatus />}
+            </div>
+          </td>
+          <td className="Settings-tbody_data">
+            <p className="Settings-tbody_date">01/04/23</p>
+          </td>
+          <td className="Settings-tbody_data">
+            <p className="Settings-tbody_creator">Admin</p>
+          </td>
+          <td className="Settings-tbody_data">
+            <TbEdit className="Edit-icon" />
+          </td>
+ 
+        </tr>
+
+
+        <tr>
+          <td className="Settings-tbody_data">
+            <div className="Settings-tbody_model">
+              <div
+                onClick={() =>
+                  handleActiveToggle3(toggleActive3, setToggleActive3)
+                }
+              >
+                {toggleActive3 ? (
+                  <BsToggleOff className="ToggleOff-icon" />
+                ) : (
+                  <BsToggleOn className="ToggleOn-icon" />
+                )}
+              </div>
+              <div>
+                <p className="Settings-tbody_model_title">Credit First Model</p>
+                <p className="Settings-tbody_model_subtitle">
+                  Credit Score Main
+                </p>
+              </div>
+            </div>
+          </td>
+          <td className="Settings-tbody_data">
+            <div
+              onClick={() =>
+                handleActiveToggle3(toggleActive3, setToggleActive3)
+              }
+            >
+              {toggleActive3 ? <InactiveStatus /> : <ActiveStatus />}
+            </div>
+          </td>
+          <td className="Settings-tbody_data">
+            <p className="Settings-tbody_date">01/04/23</p>
+          </td>
+          <td className="Settings-tbody_data">
+            <p className="Settings-tbody_creator">Gbenga</p>
+          </td>
+          <td className="Settings-tbody_data">
+            <TbEdit className="Edit-icon" />
+          </td>
+
+        </tr>
+
+
+        <tr>
+          <td className="Settings-tbody_data">
+            <div className="Settings-tbody_model">
+              <div
+                onClick={() =>
+                  handleActiveToggle4(toggleActive4, setToggleActive4)
+                }
+              >
+                {toggleActive4 ? (
+                  <BsToggleOff className="ToggleOff-icon" />
+                ) : (
+                  <BsToggleOn className="ToggleOn-icon" />
+                )}
+              </div>
+              <div>
+                <p className="Settings-tbody_model_title">CSI Model</p>
+                <p className="Settings-tbody_model_subtitle">
+                  Credit Score only
+                </p>
+              </div>
+            </div>
+          </td>
+          <td className="Settings-tbody_data">
+            <div
+              onClick={() =>
+                handleActiveToggle4(toggleActive4, setToggleActive4)
+              }
+            >
+              {toggleActive4 ? <InactiveStatus /> : <ActiveStatus />}
+            </div>
+          </td>
+          <td className="Settings-tbody_data">
+            <p className="Settings-tbody_date">01/04/23</p>
+          </td>
+          <td className="Settings-tbody_data">
+            <p className="Settings-tbody_creator">Gbenga</p>
+          </td>
+          <td className="Settings-tbody_data">
+            <TbEdit className="Edit-icon" />
+          </td>
+
         </tr>
       </tbody>
     </table>
--- src/Pages/Dashboard/Settings/NotificationSettings.jsx
+++ src/Pages/Dashboard/Settings/NotificationSettings.jsx
@@ -4,9 +4,27 @@ import { BsToggleOff, BsToggleOn } from "react-icons/bs";
 export default function NotificationSettings() {
   const [toggle1, setToggle1] = useState(false);
   const [toggle2, setToggle2] = useState(false);
+  const [toggle3, setToggle3] = useState(false);
+  const [toggle4, setToggle4] = useState(false);
+  const [toggle5, setToggle5] = useState(false);
+  const [toggle6, setToggle6] = useState(false);
+  const [toggle7, setToggle7] = useState(false);
+  const [toggle8, setToggle8] = useState(false);
 
   const handleToggle = (toggle, setToggle) => {
     setToggle(!toggle);
+  };
+
+  const handleToggle2 = (toggle2, setToggle2) => {
+    setToggle2(!toggle2);
+  };
+
+  const handleToggle3 = (toggle3, setToggle3) => {
+    setToggle3(!toggle3);
+  };
+
+  const handleToggle4 = (toggle4, setToggle4) => {
+    setToggle4(!toggle4);
   };
 
   return (
@@ -42,6 +60,80 @@ export default function NotificationSettings() {
             )}
           </td>
         </tr>
+
+
+        <tr>
+          <td className="Settings-tbody_data">New Loan Status</td>
+          <td
+            onClick={() => handleToggle2(toggle3, setToggle3)}
+            className="Settings-tbody_icon"
+          >
+            {toggle3 ? (
+              <BsToggleOff className="ToggleOff-icon" />
+            ) : (
+              <BsToggleOn className="ToggleOn-icon" />
+            )}
+          </td>
+          <td
+            onClick={() => handleToggle2(toggle4, setToggle4)}
+            className="Settings-tbody_icon"
+          >
+            {toggle4 ? (
+              <BsToggleOff className="ToggleOff-icon" />
+            ) : (
+              <BsToggleOn className="ToggleOn-icon" />
+            )}
+          </td>
+        </tr>
+
+        <tr>
+          <td className="Settings-tbody_data">Loan Due Date</td>
+          <td
+            onClick={() => handleToggle3(toggle5, setToggle5)}
+            className="Settings-tbody_icon"
+          >
+            {toggle5 ? (
+              <BsToggleOff className="ToggleOff-icon" />
+            ) : (
+              <BsToggleOn className="ToggleOn-icon" />
+            )}
+          </td>
+          <td
+            onClick={() => handleToggle3(toggle6, setToggle6)}
+            className="Settings-tbody_icon"
+          >
+            {toggle6 ? (
+              <BsToggleOff className="ToggleOff-icon" />
+            ) : (
+              <BsToggleOn className="ToggleOn-icon" />
+            )}
+          </td>
+        </tr>
+
+        <tr>
+          <td className="Settings-tbody_data">Loan Repayment Date</td>
+          <td
+            onClick={() => handleToggle4(toggle7, setToggle7)}
+            className="Settings-tbody_icon"
+          >
+            {toggle7 ? (
+              <BsToggleOff className="ToggleOff-icon" />
+            ) : (
+              <BsToggleOn className="ToggleOn-icon" />
+            )}
+          </td>
+          <td
+            onClick={() => handleToggle4(toggle8, setToggle8)}
+            className="Settings-tbody_icon"
+          >
+            {toggle8 ? (
+              <BsToggleOff className="ToggleOff-icon" />
+            ) : (
+              <BsToggleOn className="ToggleOn-icon" />
+            )}
+          </td>
+        </tr>
+
       </tbody>
     </table>
   );
